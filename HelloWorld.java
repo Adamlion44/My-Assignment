abf27d118be36fb7b6be32008c8781aa8c942ecb
Adamlion44 <mahamedabdulqdir@gmail.com>
2025-01-31 02:09:39 +0100

    Hello World.
    This code is my first code

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package Lesson;
+
+public class HelloWorld
+{
+//	i use one line for coding hello world
+   public static void main(String[] arg)
+   {
+	System.out.println(" Hello World"+"\n"+ " i'm Adam Khalif.");
+   }
+}
